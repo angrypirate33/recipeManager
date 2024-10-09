eb68ac169e4da3a2b75aff805d2d75c58dcba052
Jon Schiavone <jonschiavoneucf@gmail.com>
2024-10-09 17:00:29 -0400

    Updates the Ingredient class.

--- src/main/java/com/angrypirate/models/Ingredient.java
+++ src/main/java/com/angrypirate/models/Ingredient.java
@@ -1,9 +1,52 @@
 package com.angrypirate.models;
 
+import org.bson.types.ObjectId;
+
 public class Ingredient {
+    private ObjectId id;
     private String name;
     private String quantity;
     private String unit;
 
-    // Constructors, getters, and setters
+    public Ingredient() {
+        // No-argument constructor for serialization in MongoDB.
+    }
+
+    public Ingredient(String name, String quantity, String unit) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
