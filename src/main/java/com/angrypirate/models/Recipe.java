85e3042a899ecdb1f97c0c8037a134f3927792b6
Jon Schiavone <jonschiavoneucf@gmail.com>
2024-10-09 15:10:50 -0400

    Creates basis of Recipe model and RecipeService.

--- src/main/java/com/angrypirate/models/Recipe.java
+++ src/main/java/com/angrypirate/models/Recipe.java
@@ -8,8 +8,59 @@ public class Recipe {
     private String title;
     private List<Ingredient> ingredients;
     private List<String> instructions;
-//    private NutritionalInfo nutritionalInfo;
+    private NutritionalInfo nutritionalInfo;
     private String sourceUrl;
 
-    // Constructors, getters, and setters
+    public Recipe() {
+        // No-argument constructor for serialization in MongoDB.
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(List<String> instructions) {
+        this.instructions = instructions;
+    }
+
+    public NutritionalInfo getNutritionalInfo() {
+        return nutritionalInfo;
+    }
+
+    public void setNutritionalInfo(NutritionalInfo nutritionalInfo) {
+        this.nutritionalInfo = nutritionalInfo;
+    }
+
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    public void setSourceUrl(String sourceUrl) {
+        this.sourceUrl = sourceUrl;
+    }
+
 }
--- src/main/java/com/angrypirate/services/RecipeService.java
+++ src/main/java/com/angrypirate/services/RecipeService.java
@@ -1,5 +1,56 @@
 package com.angrypirate.services;
 
+import com.angrypirate.models.Ingredient;
+import com.angrypirate.models.NutritionalInfo;
+import com.angrypirate.services.MongoDBConnection;
+import com.angrypirate.models.Recipe;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.FindIterable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecipeService {
+    private MongoCollection<Document> recipeCollection;
+
+    public RecipeService() {
+        MongoDatabase database = MongoDBConnection.getDatabase();
+        recipeCollection = database.getCollection("recipes");
+    }
+
+    public void addRecipe(Recipe recipe) {
+        Document doc = new Document("title", recipe.getTitle())
+                .append("ingredients", recipe.getIngredients())
+                .append("instructions", recipe.getInstructions())
+                .append("nutritionalInfo", recipe.getNutritionalInfo())
+                .append("sourceUrl", recipe.getSourceUrl());
+        recipeCollection.insertOne(doc);
+    }
+
+    public List<Recipe> getAllRecipes() {
+        List<Recipe> recipes = new ArrayList<>();
+        FindIterable<Document> docs = recipeCollection.find();
+        for (Document doc : docs) {
+            Recipe recipe = convertDocumentToRecipe(doc);
+            recipes.add(recipe);
+        }
+        return recipes;
+    }
+
+    private Recipe convertDocumentToRecipe(Document doc) {
+        Recipe recipe = new Recipe();
+        recipe.setId(doc.getObjectId("_id"));
+        recipe.setTitle(doc.getString("title"));
+        recipe.setIngredients((List<Ingredient>) doc.get("ingredients"));
+        recipe.setInstructions((List<String>) doc.get("instructions"));
+        recipe.setNutritionalInfo((NutritionalInfo) doc.get("nutritionalInfo"));
+        recipe.setSourceUrl(doc.getString("sourceUrl"));
+        return recipe;
+    }
 
 }
